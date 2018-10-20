b4f4b8328056fa597f310b4651bfaa80ed02a987
iDarkDuck <nevinganesan@hotmail.com>
2018-10-20 14:48:36 -0400

    Revert "Merge branch 'master' of https://github.com/iDuckDark/CPP-Project-Memory-Card-Game"
    This reverts commit f7a534e8b6246f90a889b610249f512fa268c261, reversing
    changes made to 3cc5a01a83cb9b2e491d845c40f3fc761867fa45.

--- card.h
+++ card.h
@@ -1,3 +1,7 @@
+//
+// Created by iDarkDuck on 2018-10-19.
+//
+
 #ifndef CPP_PROJECT_MEMORY_GAME_CARD_H
 #define CPP_PROJECT_MEMORY_GAME_CARD_H
 
@@ -5,33 +9,38 @@
 #include <string>
 
 using namespace std;
-enum FaceAnimal {
-    Crab, Penguin, Octopus, Turtle, Walrus
-};
+//Design a class Card which can take a face of one of the five possible animals and one of the five
+//background colours. A card must also be “printable” as one string per row with the method:
 
-enum FaceBackground {
-    Red, Green, Purple, Blue, Yellow
-};
 class Card {
 
-public:
-    friend ostream &operator<<(ostream &os, const Card &card);
+    //Note that Penguin and Red are enumeration values of type FaceAnimal and FaceBackground.
+
+    enum FaceAnimal {
+        Crab, Penguin, Octopus, Turtle, Walrus
+    };
+
+    enum FaceBackground {
+        red, green, purple, blue, yellow
+    };
+
 
 private:
+    FaceAnimal faceAnimal;
+    FaceBackground faceBackground;
+    int nRows;
+
     Card() = default;
 
+    Card(int nRows);
+
     Card(FaceAnimal faceAnimal, FaceBackground faceBackground);
-    FaceAnimal _faceAnimal;
-    FaceBackground _faceBackground;
-    int _nRows = 3;
-    char _color;
 
-    char _animal;
-    string getRow(int);
     int getNRows() const;
-    char getColor() const ;
-    char getAnimal() const;
 
+public:
+
+    friend ostream &operator<<(ostream &os, const Card &card);
 };
 
 
