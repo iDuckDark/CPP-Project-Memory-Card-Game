9183263621c3a5331753c8428922b29ac838a0f8
iDarkDuck <nevinganesan@hotmail.com>
2018-10-19 23:03:28 -0400

    test

---
+++ cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Project_Memory_Game.dir/main.cpp.o"
+  "Cpp_Project_Memory_Game.pdb"
+  "Cpp_Project_Memory_Game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Project_Memory_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Peter!" << std::endl;
     return 0;
 }
