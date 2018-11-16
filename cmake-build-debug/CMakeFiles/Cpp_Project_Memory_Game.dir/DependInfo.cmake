efd40a74c243f55f084fd72ad104e569c48d628a
iDarkDuck <nevinganesan@hotmail.com>
2018-11-16 17:34:24 -0500

    Revert "ignore windows file gitignore"
    This reverts commit 9155611ad28e260f7c16ebdaa3acbc85887f10cc.

--- cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/DependInfo.cmake
@@ -4,18 +4,18 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/board.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/board.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/card.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/card.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/carddeck.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/carddeck.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/deck.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/deck.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/game.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/game.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/main.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/main.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/player.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/player.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/reward.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/reward.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/rewarddeck.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/rewarddeck.cpp.obj"
-  "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/rules.cpp" "Y:/Documents/GitHub/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/rules.cpp.obj"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/board.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/board.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/card.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/card.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/carddeck.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/carddeck.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/deck.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/deck.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/game.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/game.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/main.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/main.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/player.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/player.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/reward.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/reward.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/rewarddeck.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/rewarddeck.cpp.o"
+  "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/rules.cpp" "/Users/idarkduck/CLionProjects/CPP-Project-Memory-Card-Game/cmake-build-debug/CMakeFiles/Cpp_Project_Memory_Game.dir/rules.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "MSVC")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
