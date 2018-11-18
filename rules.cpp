ecec3e1508c02d7a1745b45bccea066f7d6b8be3
pfarah65 <pfara039@uottawa.ca>
2018-11-18 13:46:51 -0500

    Expert Mode Layout created

--- rules.cpp
+++ rules.cpp
@@ -39,35 +39,45 @@ void Rules::expertRules(Card *card, Game &game, Letter letter, Number number) {
 }
 
 void Rules::expertOctopus(Card *card, Game &game, Letter letter, Number number) {
-    cout << "Octopus" << endl;
+    cout << "You have picked an Octopus!" << endl;
+    cout << "Pick position with an adjacent card in the same row or the same column " << endl;
+
+    //TODO Get input and implement logic
+
     // When an octopus card is turned over, the
     //card is exchanging position with an adjacent card in the same row or the same column (4-neighbourhood)
     //The adjacent card may be face up or down and will remain unchanged.
 }
 
 void Rules::expertPenguin(Card *card, Game &game, Letter letter, Number number) {
-    cout << "Penguin" << endl;
-    //If a player turns over a penguin,
-    //then this player is allowed to turn a face-up card face-down. If the penguin is the first card turned up, no
+    cout << "You have picked a Penguin!" << endl;
+    cout << "Pick a card to change face up or face down " << endl;
+    //TODO Get input and implement logic
+    //If the penguin is the first card turned up, no
     //special action will take place.
 
 }
 
 void Rules::expertWalrus(Card *card, Game &game, Letter letter, Number number) {
-    cout << "Walrus" << endl;
-//The walrus enables a player to block a face down card for the next player
-//and hence the next player must choose another location.
+    cout << "You have picked a Walrus!" << endl;
+    cout << "Pick a card to block from being chosen in the next round" << endl;
+    //TODO Get input and implement logic
 }
 
 void Rules::expertCrab(Card *card, Game &game, Letter letter, Number number) {
-    cout << "Crab" << endl;
-//The player who turns over a crab card must
-//immediately turn over another card. If that card does not fit, the player loses the current round.
+    cout << "You have picked a Crab!" << endl;
+    cout << "Pick another card. If it doesn't match, you lose the round" << endl;
+    //TODO Get input and implement logic
+
+    //The player who turns over a crab card must
+    //immediately turn over another card. If that card does not fit, the player loses the current round.
 }
 
 void Rules::expertTurtle(Card *card, Game &game, Letter letter, Number number) {
-    cout << "Turtle" << endl;
-//. Finally,
-//with the turtle the next player in the current round is skipped, i.e., the next player will not turn over a card
-//and will not lose in the next step.
+    cout << "You have picked a Turtle!" << endl;
+    cout << "Skipping next player's turn." << endl;
+    //TODO Get input and implement logic
+    //. Finally,
+    //with the turtle the next player in the current round is skipped, i.e., the next player will not turn over a card
+    //and will not lose in the next step.
 }
